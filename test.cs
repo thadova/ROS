655e5f8593d221f39d16a01c967cb95bf856949d
thanhdv <doanthanhx8@gmail.com>
2023-09-12 22:58:36 +0700

    pull down ok , nhung chua run duoc voi cs-script

--- test.cs
+++ test.cs
@@ -6,7 +6,8 @@ namespace HelloWorld
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World! Thanh here! Ok done!");    
+      Console.WriteLine("Hello World! Thanh here! Ok done!");
+      Console.WriteLine("VSC pull ok on Win 10! Chua run duoc");
     }
   }
 }
