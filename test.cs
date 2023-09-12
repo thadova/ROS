eee0aadd7b715ef37eb06aa51ab122ad495c2d5f
Lucas Doan <116198153+thadova@users.noreply.github.com>
2023-09-12 15:52:07 +0000

    gitcode pull ok, test run ok

--- test.cs
+++ test.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World! Thanh here!");    
+      Console.WriteLine("Hello World! Thanh here! Ok done!");    
     }
   }
 }
